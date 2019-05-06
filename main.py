8a23e7d65b2282af27a199e2f593264972751363
szemenyeim <szemenyeim@gmail.com>
2019-05-06 09:59:14 +0200

    Test

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from car import Car
 from vision import Vision
 from camera import Camera
+import cv2
 
 if __name__ == '__main__':
     car = Car(18,12)
     vision = Vision(0.1)
     camera = Camera()
 
-    car.start()
+    #car.start()
 
     while True:
         _ ,depth = camera.getImages()
+        cv2.imshow("depth",depth)
+        cv2.waitKey(1)
         if depth is None:
-            car.stop()
-            car.reset()
+            #car.stop()
+            #car.reset()
             break
 
         ret = vision.processFrame(depth)
         if ret:
-            car.stop()
-            car.reset()
+            #car.stop()
+            #car.reset()
             break
